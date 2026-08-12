a37272bd6060fc7caac9ee5d4074ab25cb40fadb
poojanethraa <74049500+poojanethraa@users.noreply.github.com>
2026-08-12 17:40:53 +0200

    push1

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,17 +1,10 @@
-CREATE TABLE january_jobs AS
-    SELECT *
-    FROM job_postings_fact
-    WHERE EXTRACT(MONTH FROM job_posted_date) = 1;
-
-CREATE TABLE february_jobs AS
-    SELECT *
-    FROM job_postings_fact
-    WHERE EXTRACT(MONTH FROM job_posted_date) = 2;
-
-CREATE TABLE march_jobs AS
-    SELECT *
-    FROM job_postings_fact
-    WHERE EXTRACT(MONTH FROM job_posted_date) = 3;
-
-    from job_postings_fact
-    WHERE EXTRACT(MONTH FROM job_posted_date) = 4;
+SELECT *
+FROM(
+SELECT *
+FROM january_jobs
+UNION ALL
+SELECT *
+FROM february_jobs
+UNION ALL
+SELECT *
+FROM march_jobs) AS all_jobs
